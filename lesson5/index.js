f79f0c19d9dc1708dd0d9f673e3434ca201ee61d
Ivan Tsapko <icdi7434@gmail.com>
2021-11-27 17:57:16 +0300

    lesson5: improved

--- lesson5/index.js
+++ lesson5/index.js
@@ -1,59 +1,17 @@
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
-const pug = require('pug')
+require('dotenv').config()
+const {getData} = require('./controllers/outputController')
 
-const hostname = '127.0.0.1'
-const port = 3000
-
-let executionDir = process.cwd()
-
-class FolderItem {
-    constructor(name) {
-        this.name = name
-        this.type = this.getType()
-    }
-
-    getType() {
-        const isFile = fs.lstatSync(path.resolve(executionDir, this.name)).isFile();
-        return isFile ? 'file' : 'folder'
-    }
-}
-
-const template = pug.compileFile('res/index.pug')
-const contentList = dirName => fs.readdirSync(dirName)
+const port = process.env.PORT
+const hostname = process.env.HOST_NAME
+process.env.PUBLIC_ROOT = path.resolve(process.cwd(), process.env.PUBLIC_DIR)
 
 const server = http.createServer((req, res) => {
-    let type = 'folder'
-    let name = ''
-    
-    if (req.method === 'POST') {
-        let body = ''
-        req.on('data', (data) => {body += data})
-        req.on('end', () => {
-            let params = new URLSearchParams(body.toString())
-            name = params.get('name')
-            type = params.get('type')
-    
-            if (type === 'folder') {
-                executionDir = path.resolve(executionDir, name)   
-            }
-        })
-    }
-
-    setTimeout(() => {
-        res.writeHead(200, 'Content-Type', 'text/html')
-        if (type === 'file') {
-            const readStream = fs.createReadStream(path.resolve(executionDir, name), 'utf-8')
-            readStream.pipe(res)
-        } else {
-            const itemsList = contentList(executionDir).map(content => new FolderItem(content))
-            const output = template({items: itemsList})
-            res.end(output)
-        }
-    }, 5000)
+    getData(req, res)
 })
 
 server.listen(port, hostname, () => {
-    console.log('Server is running')
+    console.log(`Server is running at ${hostname}:${port}`)
 })
